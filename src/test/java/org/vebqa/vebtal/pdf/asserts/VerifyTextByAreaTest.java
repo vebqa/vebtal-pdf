ea9e8c2a0a2a4356ab1c65f7418a1e1bbb44ec33
kdoerges <kdoerges@cycronic.de>
2017-12-05 13:48:43 +0100

    new fail test in VerifyTextByAreaTest, use hamcrest matchers

--- src/test/java/org/vebqa/vebtal/pdf/asserts/VerifyTextByAreaTest.java
+++ src/test/java/org/vebqa/vebtal/pdf/asserts/VerifyTextByAreaTest.java
@@ -1,9 +1,19 @@
 package org.vebqa.vebtal.pdf.asserts;
 
-import org.junit.Test;
+import static org.hamcrest.Matchers.containsString;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
 public class VerifyTextByAreaTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void checkThatTextIsAvailabeInSpecificArea() {
 		VerifyTextByAreaAssert.assertThat("./src/test/java/resource/Testtext_Area.pdf").hasText("This is a text.")
@@ -20,5 +30,16 @@ public class VerifyTextByAreaTest {
 	public void checkThatTextFragmentIsAvailabeInSpecificArea() {
 		VerifyTextByAreaAssert.assertThat("./src/test/java/resource/Testtext_Area.pdf").hasText("text.")
 				.atPage(1).inArea(390, 220, 25, 15).check();
+	}
+	
+	@Test
+	public void failBecauseOnlyTextFragmentIsAvailabeInSpecificArea() {
+		thrown.expect(AssertionError.class);
+		thrown.expectMessage(containsString("text."));
+
+		VerifyTextByAreaAssert.assertThat("./src/test/java/resource/Testtext_Area.pdf").hasText("This is a text.")
+				.atPage(1).inArea(390, 220, 25, 15).check();
+		
 	}	
+	
 }
