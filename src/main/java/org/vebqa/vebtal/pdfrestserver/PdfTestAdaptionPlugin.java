7054a740036559dd2d57154d903de86a7f261b29
kdoerges <kdoerges@googlemail.com>
2019-02-03 22:02:22 +0100

    add autocompletion

--- src/main/java/org/vebqa/vebtal/pdfrestserver/PdfTestAdaptionPlugin.java
+++ src/main/java/org/vebqa/vebtal/pdfrestserver/PdfTestAdaptionPlugin.java
@@ -1,17 +1,28 @@
 package org.vebqa.vebtal.pdfrestserver;
 
+import java.util.TreeSet;
+
 import org.apache.commons.configuration2.CombinedConfiguration;
 import org.vebqa.vebtal.AbstractTestAdaptionPlugin;
+import org.vebqa.vebtal.CommandAutoComplete;
+import org.vebqa.vebtal.GuiManager;
 import org.vebqa.vebtal.TestAdaptionType;
 import org.vebqa.vebtal.model.Command;
 import org.vebqa.vebtal.model.CommandResult;
 import org.vebqa.vebtal.model.CommandType;
+import org.vebqa.vebtal.sut.SutStatus;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class PdfTestAdaptionPlugin extends AbstractTestAdaptionPlugin {
 
@@ -40,7 +51,48 @@ public class PdfTestAdaptionPlugin extends AbstractTestAdaptionPlugin {
 
 	@Override
 	public Tab startup() {
-		return createTab(ID, commandList, clData);
+		Tab pdfTab = createTab(ID, commandList, clData);
+		// Add
+		TreeSet<String> allCommands = new TreeSet<String>();
+		allCommands.add("open");
+		allCommands.add("verifyText");
+		final CommandAutoComplete addCommand = new CommandAutoComplete(allCommands);
+        addCommand.setPromptText("Command");
+        addCommand.setMaxWidth(200);
+        final TextField addTarget = new TextField();
+        addTarget.setMaxWidth(200);
+        addTarget.setPromptText("Target");
+        final TextField addValue = new TextField();
+        addValue.setMaxWidth(200);
+        addValue.setPromptText("Value");
+ 
+        final Button addButton = new Button("Go");
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+            	Command newCmd = new Command(addCommand.getText(), addTarget.getText(), addValue.getText());
+            	
+                addCommand.clear();
+                addTarget.clear();
+                addValue.clear();
+                
+                PdfResource aResource = new PdfResource();
+                GuiManager.getinstance().setTabStatus(PdfTestAdaptionPlugin.ID, SutStatus.CONNECTED);
+                aResource.execute(newCmd);
+                GuiManager.getinstance().setTabStatus(PdfTestAdaptionPlugin.ID, SutStatus.DISCONNECTED);
+            }
+        });
+ 
+        HBox hbox = new HBox();
+         
+        hbox.getChildren().addAll(addCommand, addTarget, addValue, addButton);
+        hbox.setSpacing(3);
+
+		BorderPane pane = (BorderPane)pdfTab.getContent();
+		pane.setTop(hbox);        
+
+		
+		return pdfTab;
 	}
 
 	public static void addCommandToList(Command aCmd, CommandType aType) {
