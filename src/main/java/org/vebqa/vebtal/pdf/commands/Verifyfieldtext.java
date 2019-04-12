61c57279ef91f313457d62574f5cb767c0965724
kdoerges <karsten.doerges@consorsfinanz.de>
2019-04-12 09:38:30 +0200

    update dependency, add new command verifyFieldText

---
+++ src/main/java/org/vebqa/vebtal/pdf/commands/Verifyfieldtext.java
@@ -0,0 +1,54 @@
+package org.vebqa.vebtal.pdf.commands;
+
+import org.vebqa.vebtal.annotations.Keyword;
+import org.vebqa.vebtal.command.AbstractCommand;
+import org.vebqa.vebtal.model.CommandType;
+import org.vebqa.vebtal.model.Response;
+import org.vebqa.vebtal.pdf.PDFDriver;
+import org.vebqa.vebtal.pdfrestserver.PdfTestAdaptionPlugin;
+
+@Keyword(module = PdfTestAdaptionPlugin.ID, command = "verifyFieldText", hintTarget = "name=<partial name>", hintValue = "<text>")
+public class Verifyfieldtext extends AbstractCommand {
+	
+	public Verifyfieldtext(String aCommand, String aTarget, String aValue) {
+		super(aCommand, aTarget, aValue);
+		this.type = CommandType.ASSERTION;
+	}
+
+	@Override
+	public Response executeImpl(Object driver) {
+		PDFDriver pdfDriver = (PDFDriver)driver;
+
+		Response tResp = new Response();
+		
+		String name = "";
+		String qualified = "";
+		
+		String[] parts = this.target.split(";");
+		for (String part : parts) {
+			String[] subParts = part.split("=");
+			switch (subParts[0]) {
+			case "name":
+				name = subParts[1];
+				break;
+			case "qualified":
+				qualified = subParts[1];
+				break;
+
+			default:
+				break;
+			}
+		}		
+		
+		String tValue = pdfDriver.getValueByFieldName(name);
+		if (tValue != null && tValue.contentEquals(this.value)) {
+			tResp.setCode(Response.PASSED);
+			tResp.setMessage("value of field matches given text: " + this.value);
+		} else {
+			tResp.setCode(Response.FAILED);
+			tResp.setMessage("Field should be " + this.value + ", but is: " + value);
+		}
+		
+		return tResp;
+	}
+}
